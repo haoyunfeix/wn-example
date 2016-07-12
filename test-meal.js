1388dfb35ff0b63f30d3a8877f2390622f220681
Kenny Yuan <kaining.yuan@intel.com>
2016-07-12 16:26:12 +0800

    Trivial improvement of test-meal.js -- adding progress when waiting for promise to be resolved

--- test-meal.js
+++ test-meal.js
@@ -3,17 +3,21 @@ var addon = require('bindings')('exampleAddon');
 var x = new addon.Meal();
 
 x.prepare("ramen", 0.5);
-console.log(x.type + ", " + x.size);
+console.log("Meal type is: " + x.type + ", and weight in " + x.size + 'kg');
 
 console.log("raw meal? " + (x.isRawMeal ? "Yes" : "Not any more"));
 
-console.log("Waiting for promise...");
+console.log('Call cook() method and waiting for promise...');
+
+var timer = setInterval(function() {
+  process.stdout.write('.');
+}, 100);
 
 x.cook("Trump").then(
   value => {
-    console.log("Resolved as: " + value);
+    console.log(value);
     console.log("raw meal? " + (x.isRawMeal ? "Yes" : "Not any more"));
-  	
+    clearInterval(timer);
   }
 );
 console.log("raw meal? " + (x.isRawMeal ? "Yes" : "Not any more"));
